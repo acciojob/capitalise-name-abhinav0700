7e3a2c14eeaa1264deec05944cba3102302ef969
Abhinav M <130998983+abhinav0700@users.noreply.github.com>
2025-08-31 20:59:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+
+function toUppercase() {
+      let input = document.getElementById("fname");
+      input.value = input.value.toUpperCase();
+    }
+
+    // Attach event listener for when the field loses focus
+document.getElementById("fname").addEventListener("blur", toUppercase);
+  
